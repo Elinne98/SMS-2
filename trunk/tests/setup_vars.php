ab400be961ef5091f9e6ccd1f998e7bbbaf89320
cristeab <cristeab@355a985f-30d4-43a6-976f-d6b10e5026c3>
2011-07-24 12:51:03 +0000

    release 2.1.2

--- trunk/tests/setup_vars.php
+++ trunk/tests/setup_vars.php
@@ -1,7 +1,7 @@
 <?php
-define('FRESMS_BASE_URL', 'http://localhost/freesms_dev');
+define('FRESMS_BASE_URL', 'http://localhost/~bogdan/FreeSMS');
 define('DATABASE_NAME', 'crcdb');
-define('MYSQL_ROOT_PASSWORD', 'ewigkeit');
+define('MYSQL_ROOT_PASSWORD', 'Aephae4A');
 define('USER_NAME', 'bogdan');
 define('USER_PASSWORD', 'ewigkeit');
 ?>
